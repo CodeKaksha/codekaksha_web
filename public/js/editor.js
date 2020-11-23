668f0c67136b83cf1f10fdc9c106ed351073a4b3
Abhinandan <abhi.moudgil15@gmail.com>
2020-11-23 18:07:08 +0530

    Added event listener for editor

--- public/js/editor.js
+++ public/js/editor.js
@@ -1,17 +1,33 @@
 function getEl(id) {
   return document.getElementById(id);
 }
-window.addEventListener("onload", () => {
-  const editor = document.querySelector(".ace_content");
-  editor.addEventListener("keyup", (evt) => {
-    const text = editor.value;
-    socket.send(text);
+let editor;
+let bruh;
+window.onload=() => {
+  const targetNode = document.getElementById(".ace_text-layer");
+  let j=1;
+  // Options for the observer (which mutations to observe)
+  const config = { attributes: true, childList: false, subtree: false };
+
+  if(j)
+  {
+    bruh=editor
+    j=0;
+  }
+  const observer = new MutationObserver(()=>{
+  
+    if(bruh!=editor.innerHTML)
+    {
+      socket.send(editor.innerHTML);
+    }
   });
-});
+  observer.observe(targetNode, config);
+};
 socket.on("message", (data) => {
-  editor.value = data;
+  editor.innerHTML = data;
+  bruh=data;
 });
 
 var e = ace.edit("jsEditor");
-e.getSession().setMode("ace/mode/javascript");
+e.getSession().setMode("ace/mode/c_cpp");
 e.setTheme("ace/theme/terminal");
