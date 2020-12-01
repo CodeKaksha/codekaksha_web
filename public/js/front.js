e71fb7a90a149a9805f9b4d189d4d62ab4cc9318
Abhinandan <abhi.moudgil15@gmail.com>
2020-12-01 23:51:59 +0530

    added loader

--- public/js/front.js
+++ public/js/front.js
@@ -1,15 +1,25 @@
-let bigDiv=document.querySelector('.big_div');
-let diff_height=-71;
-window.setInterval(()=>{
-    bigDiv.style.transform=`translateY(${diff_height}px)`;
-    diff_height-=71;
-    if(diff_height<-214)
-    {
-        diff_height=0;
-    }
-},3000)
+let loader = document.querySelector(".loader");
+let bigDiv = document.querySelector(".big_div");
+let diff_height = -71;
+window.setInterval(() => {
+  bigDiv.style.transform = `translateY(${diff_height}px)`;
+  diff_height -= 71;
+  if (diff_height < -214) {
+    diff_height = 0;
+  }
+}, 3000);
 
-$(document).ready(function(){
-    $('#modal1').modal();
-    $('#modal2').modal();
-  });
+$(document).ready(function () {
+  $("#modal1").modal();
+  $("#modal2").modal();
+});
+
+function loaderOn() {
+    loader.classList.remove("hidden");
+    loader.classList.remove("fade_out");
+  loader.classList.add("fade_in");
+}
+function loaderOut() {
+    loader.classList.remove("fade_in");
+    loader.classList.add("fade_out");
+}
