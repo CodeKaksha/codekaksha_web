bdfcc67ed4a8e461977d595c14b6c26218e4a8a7
Abhinandan <abhi.moudgil15@gmail.com>
2020-12-01 15:51:21 +0530

    Added word fade-in-out effect

---
+++ public/js/front.js
@@ -0,0 +1,10 @@
+let bigDiv=document.querySelector('.big_div');
+let diff_height=-71;
+window.setInterval(()=>{
+    bigDiv.style.transform=`translateY(${diff_height}px)`;
+    diff_height-=71;
+    if(diff_height<-214)
+    {
+        diff_height=0;
+    }
+},3000)
